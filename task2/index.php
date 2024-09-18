b2f66e1a36ef2bf84214f716ba77186bf21c28a2
TQVLucKy <tranquanvinh882003@gmail.com>
2024-09-18 23:15:17 +0700

    header

---
+++ task2/index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="header-left">
+                <ul>
+                    <li class="header-list-left">
+                        <a class="header-list-items" href="">MENS</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">EDITS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">The Holiday Edit</a></li>
+                                        <li><a href="">Colours Of Spring</a></li>
+                                        <li><a href="">Best Of Linen</a></li>
+                                        <li><a href="">Spring Textures</a></li>
+                                        <li><a href="">Academy Icons</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">CLOTHING</a>
+                                    <ul class="linklist">
+                                        <li><a href="">View All</a></li>
+                                        <li><a href="">Shorts</a></li>
+                                        <li><a href="">Long Sleeve Shirts</a></li>
+                                        <li><a href="">Short Sleeve Shirts</a></li>
+                                        <li><a href="">Polo Shirts</a></li>
+                                        <li><a href="">Tops & Tees</a></li>
+                                        <li><a href="">Swim</a></li>
+                                        <li><a href="">Pants & Chinos</a></li>
+                                        <li><a href="">Long Sleeve Tops</a></li>
+                                        <li><a href="">Sweats</a></li>
+                                        <li><a href="">Jackets & Outerwear</a></li>
+                                        <li><a href="">Knitwear</a></li>
+                                        <li><a href="">Accessories</a></li>
+                                        <li><a href="">Sale</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_f19f0c19-244e-493e-ad9d-c8623bec0190_370x230@2x.jpg?v=1725940076">
+                                    <b>NEW ARRIVALS</b>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_GetawayEdit_370x230@2x.jpg?v=1726616398">
+                                    <b>THE GETAWAY EDIT</b>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="header-list-left">
+                        <a class="header-list-items" href="">WOMENS</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">EDITS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">The Getaway Edit</a></li>
+                                        <li><a href="">The Colour Edit</a></li>
+                                        <li><a href="">Live In Linen</a></li>
+                                        <li><a href="">Best Sellers</a></li>
+                                        <li><a href="">Academy Capsule</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">CLOTHING</a>
+                                    <ul class="linklist">
+                                        <li><a href="">View All</a></li>
+                                        <li><a href="">New Arrivals</a></li>
+                                        <li><a href="">Shorts</a></li>
+                                        <li><a href="">Short Sleeve Shirts</a></li>
+                                        <li><a href="">Long Sleeve Shirts</a></li>
+                                        <li><a href="">Tops & Tees</a></li>
+                                        <li><a href="">Sets</a></li>
+                                        <li><a href="">Dresses</a></li>
+                                        <li><a href="">Pants</a></li>
+                                        <li><a href="">Sweats</a></li>
+                                        <li><a href="">Knitwear</a></li>
+                                        <li><a href="">Jackets</a></li>
+                                        <li><a href="">Long Sleeve Tops</a></li>
+                                        <li><a href="">Accessories</a></li>
+                                        <li><a href="">Sale</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S3_c9678eca-06af-407c-bffc-18b0cf9ee5c6_370x230@2x.jpg?v=1725940140">
+                                    <b>NEW ARRIVALS</b>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_GetawayEdit2_370x230@2x.jpg?v=1726616422">
+                                    <b>THE GETAWAY EDIT</b>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="header-list-left"><a class="header-list-items" href="">KIDS</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">Academy Kids</a>
+                                    <ul class="linklist">
+                                        <li><a href="">View All</a></li>
+                                        <li><a href="">New Arrivals</a></li>
+                                        <li><a href="">Shirts</a></li>
+                                        <li><a href="">Shorts</a></li>
+                                        <li><a href="">Tees</a></li>
+                                        <li><a href="">Swim</a></li>
+                                        <li><a href="">Sets</a></li>
+                                        <li><a href="">Pants</a></li>
+                                        <li><a href="">Tops</a></li>
+                                        <li><a href="">Sweats</a></li>
+                                        <li><a href="">Jackets</a></li>
+                                        <li><a href="">Sale</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="https://academybrand.com/cdn/shop/files/Megamenu_25S5_370x230@2x.jpg?v=1725940169">
+                                    <b>NEW ARRIVALS</b>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="https://academybrand.com/cdn/shop/files/Megamenu_25S6_370x230@2x.jpg?v=1725940199">
+                                    <b>KIDS TEES</b>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="header-list-left"><a class="header-list-items" href="">ACCESSORIES</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">EDITS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">The Holiday Edit</a></li>
+                                        <li><a href="">Colours Of Spring</a></li>
+                                        <li><a href="">Best Of Linen</a></li>
+                                        <li><a href="">Spring Textures</a></li>
+                                        <li><a href="">Academy Icons</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">CLOTHING</a>
+                                    <ul class="linklist">
+                                        <li><a href="">View All</a></li>
+                                        <li><a href="">Shorts</a></li>
+                                        <li><a href="">Long Sleeve Shirts</a></li>
+                                        <li><a href="">Short Sleeve Shirts</a></li>
+                                        <li><a href="">Polo Shirts</a></li>
+                                        <li><a href="">Tops & Tees</a></li>
+                                        <li><a href="">Swim</a></li>
+                                        <li><a href="">Pants & Chinos</a></li>
+                                        <li><a href="">Long Sleeve Tops</a></li>
+                                        <li><a href="">Sweats</a></li>
+                                        <li><a href="">Jackets & Outerwear</a></li>
+                                        <li><a href="">Knitwear</a></li>
+                                        <li><a href="">Accessories</a></li>
+                                        <li><a href="">Sale</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_f19f0c19-244e-493e-ad9d-c8623bec0190_370x230@2x.jpg?v=1725940076">
+                                    <b>NEW ARRIVALS</b>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_GetawayEdit_370x230@2x.jpg?v=1726616398">
+                                    <b>THE GETAWAY EDIT</b>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="header-list-left"><a class="header-list-items" href="">SALE</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">EDITS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">The Holiday Edit</a></li>
+                                        <li><a href="">Colours Of Spring</a></li>
+                                        <li><a href="">Best Of Linen</a></li>
+                                        <li><a href="">Spring Textures</a></li>
+                                        <li><a href="">Academy Icons</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">CLOTHING</a>
+                                    <ul class="linklist">
+                                        <li><a href="">View All</a></li>
+                                        <li><a href="">Shorts</a></li>
+                                        <li><a href="">Long Sleeve Shirts</a></li>
+                                        <li><a href="">Short Sleeve Shirts</a></li>
+                                        <li><a href="">Polo Shirts</a></li>
+                                        <li><a href="">Tops & Tees</a></li>
+                                        <li><a href="">Swim</a></li>
+                                        <li><a href="">Pants & Chinos</a></li>
+                                        <li><a href="">Long Sleeve Tops</a></li>
+                                        <li><a href="">Sweats</a></li>
+                                        <li><a href="">Jackets & Outerwear</a></li>
+                                        <li><a href="">Knitwear</a></li>
+                                        <li><a href="">Accessories</a></li>
+                                        <li><a href="">Sale</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_f19f0c19-244e-493e-ad9d-c8623bec0190_370x230@2x.jpg?v=1725940076">
+                                    <b>NEW ARRIVALS</b>
+                                </div>
+                                <div class="megamenu-img">
+                                    <img srcset="//academybrand.com/cdn/shop/files/Megamenu_25S_GetawayEdit_370x230@2x.jpg?v=1726616398">
+                                    <b>THE GETAWAY EDIT</b>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="header-list-left"><a class="header-list-items" href="">DEALS</a>
+                        <div class="dropdown-content">
+                            <div class="megamenu-list">
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">MENS DEALS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">2 for $99 Volleys</a></li>
+                                        <li><a href="">2 for $60 Roth & Jimmy Tees</a></li>
+                                        <li><a href="">2 for $50 Basic Tees</a></li>
+                                        <li><a href="">2 for $80 Mac & Varsity Tees</a></li>
+                                        <li><a href="">2 for $80 Workers</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">WOMENS DEALS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">2 for $60 Tees & Tanks</a></li>
+                                        <li><a href="">2 for $80 Long Sleeve Tees</a></li>
+                                    </ul>
+                                </div>
+                                <div class="megamenu-item">
+                                    <a class="megamenu-item-title" href="">ROOKIE DEALS</a>
+                                    <ul class="linklist">
+                                        <li><a href="">2 for $30 Tees</a></li>
+                                        <li><a href="">2 for $80 Sweats</a></li>
+                                        <li><a href="">2 for $80 Vintage Oxfords</a></li>
+                                        <li><a href="">2 for $50 Workers</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="header-center">
+                <img srcset="//academybrand.com/cdn/shop/files/TAB_Logo_Stacked_Black_160x.png?v=1649579160 1x, //academybrand.com/cdn/shop/files/TAB_Logo_Stacked_Black_160x@2x.png?v=1649579160 2x">
+            </div>
+            <div class="header-right">
+                <ul>
+                    <li><a>SEARCH</a></li>
+                    <li><a>STORES</a></li>
+                    <li><a>ACCOUNT</a></li>
+                    <li><a>REWARDS</a></li>
+                    <li><a>WISHLIST</a></li>
+                    <li><a>CART(0)</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main">
+            <div class="slideshow">
+                <div class="slideshow-content">
+                    <p>A FRESH DROP HAS LANDED</p>
+                    <h2>NEW SPRING</h2>
+                    <h2>ARRIVALS</h2>
+                </div>
+                <div class="slideshow-button">
+                    <a href="" class="buttongroup-item">SHOP MENS</a>
+                    <a href="" class="buttongroup-item">SHOP WOMENS</a>
+                </div>
+            </div>
+            <div class="slideshow-image">
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footer-left">
+                <div class="linklist-container">
+                    <h2>DISCOVER</h2>
+                    <ul>
+                        <li>Stores</li>
+                        <li>Gift Cards</li>
+                        <li>Check Gift Card Balance</li>
+                        <li>Size Guide</li>
+                        <li>The Journal</li>
+                    </ul>
+                </div>
+                <div class="linklist-container">
+                    <h2>HELP</h2>
+                    <ul>
+                        <li>Contact Us</li>
+                        <li>Help Centre</li>
+                        <li>Shipping</li>
+                        <li>Returns</li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="footer-right">
+                <h2>ACADEMY REWARDS</h2>
+                <p>Poin Academy Rewards to receive a $10 Welcome reward and earn as you spend. Please allow 24 hours for rewards to appear in your account.</p>
+                <a href="">JOIN NOW</a>
+                <ul class="footer-social">
+                    <li><a href=""><i class="fa fa-facebook-official"></i></a></li>
+                    <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
